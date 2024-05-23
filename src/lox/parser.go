aa32388b68ecebec384485adcd75847325075133
Shengkai Cheng <skcheng003@gmail.com>
2024-05-23 13:39:43 +0800

    add parser.go file

--- src/lox/parser.go
+++ src/lox/parser.go
@@ -1,1 +1,22 @@
 package lox
+
+type Parser struct {
+	tokens  []Token
+	current int
+}
+
+func NewParser(tokens []Token) *Parser {
+	return &Parser{
+		tokens:  tokens,
+		current: 0,
+	}
+}
+
+func (p *Parser) expression() Expr {
+	return p.equality()
+}
+
+func (p *Parser) equality() Expr {
+	return nil
+
+}
